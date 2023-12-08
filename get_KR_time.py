df6852b5a7a247ceb7f261d8fbfbc546165152fa
Hyuk-San Kwon <95512555+Hyuk-San-Kwon@users.noreply.github.com>
2023-12-08 18:59:26 +0900

    Create get_KR_time.py

---
+++ get_KR_time.py
@@ -0,0 +1,67 @@
+import network
+import socket
+import time
+import struct
+import machine
+import json
+
+from machine import Pin
+
+NTP_DELTA = 2208988800
+host = "pool.ntp.org"
+
+led = Pin("LED", Pin.OUT)
+WIFI_FILE = "wifi.json"
+
+
+def set_time():
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1B
+    addr = socket.getaddrinfo(host, 123)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.settimeout(1)
+        res = s.sendto(NTP_QUERY, addr)
+        msg = s.recv(48)
+    finally:
+        s.close()
+    val = struct.unpack("!I", msg[40:44])[0]
+    t = val - NTP_DELTA    
+    tm = time.gmtime(t)
+    machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3] + 9, tm[4], tm[5], 0))
+
+def get_time():
+    
+    global WIFI_FILE
+    
+    with open(WIFI_FILE) as f:
+        wifi_credentials = json.load(f)
+    f.close()
+
+    WIFI_ID = wifi_credentials["ssid"]
+    WIFI_PWD = wifi_credentials["password"]
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(WIFI_ID, WIFI_PWD)
+
+    max_wait = 15
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        max_wait -= 1
+        print('waiting for connection...')
+        time.sleep(1)
+
+    if wlan.status() != 3:
+        raise RuntimeError('network connection failed')
+    else:
+        print('connected')
+        status = wlan.ifconfig()
+        print( 'ip = ' + status[0] )
+        
+    set_time()
+
+# led.on()
+# 
+# print(time.localtime())
+# led.off()
